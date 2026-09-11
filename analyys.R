d2fc6a89ff2abf658ffcb25db492a98d9dc7d422
Sirle Sõstra-Oru <sirle.sostra-oru@ut.ee>
2026-09-11 10:23:10 +0300

    Lisa mediaanide arvutus

--- analyys.R
+++ analyys.R
@@ -1,4 +1,6 @@
 # Loeb proovide tabeli ja arvutab ekspressiooni keskmise gruppide kaupa
 proovid <- read.csv("proovid.csv")
 keskmised <- aggregate(ekspressioon ~ grupp, data = proovid, FUN = mean)
+mediaanid <- aggregate(ekspressioon ~ grupp, data = proovid, FUN = median)
+print(mediaanid)
 print(keskmised)
